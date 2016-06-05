5727b1b4847c3e9e80827206ab2abf26bebef78a
marciooliveir1 <marciooliveir1@gmail.com>
2016-06-05 14:34:55 -0300

    Update index.php
    criação do arquivo index

--- index.php
+++ index.php
@@ -1,38 +1,22 @@
-<?php
-require('conexao.php');
-?>
-<html>
-<head>
-<title>noticias.php</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<style>
-#n1 { font-family:verdana,arial,tahoma,times new roman; font-size:10pt; color:black; font-weight:bold; }
-#n2 { font-family:verdana,arial,tahoma,times new roman; font-size:7pt; color:gray; }
-#n3 { font-family:verdana,arial,tahoma,times new roman; font-size:8pt; color:black; }
-</style>
-</head>
+<?php 
+require ('include/principal.php');
+require ('classes/noticia.class.php');
 
-<body>
-<form action="noticia.php" method="post">
-            <fieldset>
-                <label for="time">Data: </label>
-                <input required name="info_time" type="date">
-            </fieldset>
-            <fieldset>
-                <label for="">Titulo: </label>
-                <input required name="info_title" type="text">
-            </fieldset>
-            <fieldset>
-                <label for="texto">Noticia: </label>
-                <textarea required name="info_text" type="text"></textarea> 
-            </fieldset>
-            <fieldset>
-                <label for="foto">Foto: </label>
-                <input required name="foto" type="file">
-            </fieldset>
-            <fieldset>
-                <button type="submit">Enviar</button>
-            </fieldset>
-        </form>
+$noticia = new Noticia();
+
+$noticias = $noticia->getAll();
+
+ ?>
+    <body>
+    <center>
+        <?php foreach ($noticias as $not): ?>
+                <a href="noticia.php?id=<?=$not['id_noticia']?>">
+                    <img src="imgnoticias/<?=$not['id_noticia']?>.jpg" width="120" height="90" alt="">
+                    <span></span>
+                </a>
+                <a href="noticia.php?id=<?=$not['id_noticia']?>"><?=$not['gravata']?>...</a></p>
+                <p><?=$not['data']?></p>
+        <?php endforeach; ?> 
+        </center>            
     </body>
 </html>
