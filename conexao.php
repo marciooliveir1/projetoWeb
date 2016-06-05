f31c83be839866709bea896f7e3db01f85319429
marciooliveir1 <marciooliveir1@gmail.com>
2016-06-05 14:31:28 -0300

    Delete conexao.php
    exclusao de arquivo

--- conexao.php
+++
@@ -1,4 +0,0 @@
-<?php
-$string_connect = "host=$host port=$port dbname=$db user=$user password=$pass"; // Variável com as informações para a conexão ser feita
-$connect = pg_pconnect($string_connect); // Comando para conectar-se ao servidor
-?>
